ea6321b7491cbf79de42a945207aa4724d582423
Greenacre <neil.muggleton@gmail.com>
2025-06-06 12:02:52 +0800

    Initial commit via file upload

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+//import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Header from "./components/Header.jsx"
+import MainContent from "./components/MainContent.jsx"
+import Footer from "./components/Footer.jsx"
+import './index.css'
+
+const root = createRoot(document.getElementById("root"))
+
+function Page() {
+    return (
+	    <>
+  			<Header />
+	  		<MainContent />
+		  	<Footer />
+	    </>
+    )
+}
+
+root.render(
+    <Page />
+)
